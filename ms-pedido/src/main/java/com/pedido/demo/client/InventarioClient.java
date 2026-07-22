26eccb3d6544f3a36c97be7a04d5707921251ebd
nildarojas <nildarc04@gmail.com>
2026-07-22 17:29:21 -0500

    completado

--- ms-pedido/src/main/java/com/pedido/demo/client/InventarioClient.java
+++ ms-pedido/src/main/java/com/pedido/demo/client/InventarioClient.java
@@ -4,9 +4,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "InventarioClient", url = "http://localhost:8083/v1/productos")
+@FeignClient(
+        name = "InventarioClient",
+        url = "${INVENTARIO_URL:http://localhost:8083/v1/productos}"
+)
 public interface InventarioClient {
 
     @GetMapping("/verificar")
-    boolean verificar(@RequestParam("codigo") String codigo, @RequestParam("pasillo") String pasillo);
+    boolean verificar(
+            @RequestParam("codigo") String codigo,
+            @RequestParam("pasillo") String pasillo
+    );
 }
