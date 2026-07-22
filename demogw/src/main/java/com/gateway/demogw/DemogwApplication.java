30dd90ad525fabd29809f0e0404b2e04a7864fa1
nildarojas <nildarc04@gmail.com>
2026-07-22 01:40:31 -0500

    subida actualizada de proyecto

---
+++ demogw/src/main/java/com/gateway/demogw/DemogwApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.demogw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogwApplication.class, args);
+	}
+
+}
---
+++ demogw/src/test/java/com/gateway/demogw/DemogwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.demogw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
